91dac3628a4ce9c362feb159f949388a05d2d094
Dobubu <rilakkuma0330k@gmail.com>
2015-12-15 11:48:08 +0800

    淡水、報告、課程、閱讀紀錄
    增加照片和資料

---
+++ devServer/data/reading.js
@@ -0,0 +1,118 @@
+var data = {
+	reading: [
+		{
+			img: './asset/images/reading/reading_101103302_1.jpg',
+			stuId : ['101103302']
+		},
+		{
+			img: './asset/images/reading/reading_101103302_2.jpg',
+			stuId : ['101103302']
+		},
+		{
+			img: './asset/images/reading/reading_101109205_1.jpg',
+			stuId : ['101109205']
+		},
+		{
+			img: './asset/images/reading/reading_101109205_2.jpg',
+			stuId : ['101109205']
+		},
+		{
+			img: './asset/images/reading/reading_102103143.jpg',
+			stuId : ['102103143']
+		},
+		{
+			img: './asset/images/reading/reading_102103146.jpg',
+			stuId : ['102103146']
+		},
+		{
+			img: './asset/images/reading/reading_102103148_1.jpg',
+			stuId : ['102103148']
+		},
+		{
+			img: './asset/images/reading/reading_102103148_2.jpg',
+			stuId : ['102103148']
+		},
+		{
+			img: './asset/images/reading/reading_102103148_3.jpg',
+			stuId : ['102103148']
+		},
+		{
+			img: './asset/images/reading/reading_102103152.jpg',
+			stuId : ['102103152']
+		},
+		{
+			img: './asset/images/reading/reading_102103154.jpg',
+			stuId : ['102103154']
+		},
+		{
+			img: './asset/images/reading/reading_102103239.jpg',
+			stuId : ['102103239']
+		},
+		{
+			img: './asset/images/reading/reading_102103324.jpg',
+			stuId : ['102103324']
+		},
+		{
+			img: './asset/images/reading/reading_102103340.jpg',
+			stuId : ['102103340']
+		},
+		{
+			img: './asset/images/reading/reading_102103355_1.jpg',
+			stuId : ['102103355']
+		},
+		{
+			img: './asset/images/reading/reading_102103355_2.jpg',
+			stuId : ['102103355']
+		},
+		{
+			img: './asset/images/reading/reading_102104251.jpg',
+			stuId : ['102104251']
+		},
+		{
+			img: './asset/images/reading/reading_102105106.jpg',
+			stuId : ['102105106']
+		},
+		{
+			img: './asset/images/reading/reading_102105107.jpg',
+			stuId : ['102105107']
+		},
+		{
+			img: './asset/images/reading/reading_102105128_1.jpg',
+			stuId : ['102105128']
+		},
+		{
+			img: './asset/images/reading/reading_102105128_2.jpg',
+			stuId : ['102105128']
+		},
+		{
+			img: './asset/images/reading/reading_102105134_1.jpg',
+			stuId : ['102105134']
+		},
+		{
+			img: './asset/images/reading/reading_102105134_2.jpg',
+			stuId : ['102105134']
+		},
+		{
+			img: './asset/images/reading/reading_102105157_1.jpg',
+			stuId : ['102105157']
+		},
+		{
+			img: './asset/images/reading/reading_102105157_2.jpg',
+			stuId : ['102105157']
+		},
+		{
+			img: './asset/images/reading/reading_102105333_1.jpg',
+			stuId : ['102105333']
+		},
+		{
+			img: './asset/images/reading/reading_102105333_2.jpg',
+			stuId : ['102105333']
+		},
+		{
+			img: './asset/images/reading/reading_102109120.jpg',
+			stuId : ['102109120']
+		},
+	],
+};
+
+module.exports = data;
